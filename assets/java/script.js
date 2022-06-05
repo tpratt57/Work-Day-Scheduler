507679ced0cd2b005e3fde8b5409a352b30c1cac
unknown <pratttrevor79@gmail.com>
2022-06-05 16:28:42 -0500

    attempting to put getCurrentTime function into document.ready

--- assets/java/script.js
+++ assets/java/script.js
@@ -4,44 +4,47 @@ $("#currentDay").html(todaysDate);
 
 //function to track local time so css can display past, present, and future
 
-function getCurrentTime(){
-    //get current hour time
-    var currentTime = moment().hour();
-
-    //loop through time blocks
-    $(".time-block").each(function(){
-        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-
-        //check time and add classes for background
-        if(timeBlock < currentTime){
-            $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
-        }
-        else if(timeBlock === currentTime){
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else{
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
-});
-}
 
 //function to save button click, save text in local storage & get values of description in JQuery
 
-$(document).ready(function(){
+$(document).ready(function () {
     //"click" listener for saveBtn
-    $(".saveBtn").on("click", function(){
+    $(".saveBtn").on("click", function () {
         //get values of description from JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-    // save text to local storage
-    localStorage.setItem(time, text);
-})
+        // save text to local storage
+        localStorage.setItem(time, text);
+    })
+
+    function getCurrentTime() {
+    //get current hour time
+    var currentTime = moment().hour();
+
+    //loop through time blocks
+    $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+        //check time and add classes for background
+        if (timeBlock < currentTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+        else if (timeBlock === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+getCurrentTime();
 });
+
 
